c4b82af76c23b735b63279b1b8aa551419693a6d
yorke77 <yorke77@163.com>
2018-07-17 23:26:04 +0800

    add util class

--- app/src/main/java/com/foxconn/myapplication/MainActivity.java
+++ app/src/main/java/com/foxconn/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.foxconn.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.foxconn.myapplication.utilities.MyTextUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWelcomeString();
+
+        //Ctrl+J 补全for模块代码
+        for (int i  = 0; i < 10; i++) {
+            //F9 跳到下一次断点
+            Log.i("MainActivity", "Value:" + i);
+        }
+
+    }
+
+    private void getWelcomeString() {
+        //Shift+F6重命名变量
+        MyTextUtil myUtil = new MyTextUtil();
+
+        //Alt+Enter 列出意图行为
+        TextView tv = findViewById(R.id.myTextView);
+        tv.setText(myUtil.getValue());
     }
 }
---
+++ app/src/main/java/com/foxconn/myapplication/utilities/MyTextUtil.java
@@ -0,0 +1,10 @@
+package com.foxconn.myapplication.utilities;
+
+public class MyTextUtil {
+
+    //Ctrl+Shift+Enter 补全整个方法的结构
+    public String getValue() {
+        return "Hello from the utility class";
+    }
+
+}
